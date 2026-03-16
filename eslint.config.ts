6b2dc01ea1a0ba85338507dabd96bcc0b696add7
Rafał Majewski <goodheropl@gmail.com>
2026-03-16 11:40:14 +0000

    chore: Add commitlint

--- eslint.config.ts
+++ eslint.config.ts
@@ -1171,6 +1171,10 @@ export default [
 		},
 	},
 	{
+		files: [`./commitlint.config.ts`],
+		rules: {"import-x/no-default-export": [`off`]},
+	},
+	{
 		files: [`./eslint.config.ts`],
 		rules: {"import-x/no-default-export": [`off`]},
 	},
