92874228197190ec5116d79d9cbbc91d24fdb6c7
Rafał Majewski <goodheropl@gmail.com>
2026-03-16 13:07:36 +0000

    chore: Add release-it

---
+++ release-it.config.ts
@@ -0,0 +1,27 @@
+import type {Config} from "release-it";
+export default {
+	git: {
+		commitArgs: [],
+		commitMessage: `release: \${version}`,
+		pushArgs: [],
+		tagArgs: [],
+	},
+	hooks: {},
+	npm: {publish: false},
+	plugins: {
+		"@release-it/conventional-changelog": {
+			ignoreRecommendedBump: false,
+			preset: {
+				header: `# Changelog`,
+				name: `conventionalcommits`,
+				releaseCommitMessageFormat: `release: {{currentTag}}`,
+				types: [
+					{hidden: false, section: `Chores`, type: `chore`},
+					{hidden: false, section: `Features`, type: `feature`},
+					{hidden: false, section: `Releases`, type: `release`},
+				],
+			},
+			strictSemVer: false,
+		},
+	},
+} as const satisfies Config;
