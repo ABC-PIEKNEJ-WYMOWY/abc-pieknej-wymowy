fde89fb31df2711f4edda3e07abf116a8261312c
Rafał Majewski <goodheropl@gmail.com>
2026-03-27 17:28:09 +0000

    fixup

--- building/core/schema-for-process-env/schemaForProcessEnvOfBuilding.ts
+++ building/core/schema-for-process-env/schemaForProcessEnvOfBuilding.ts
@@ -1,3 +1,2 @@
 import {schemaForProcessEnvWithAdapter} from "../../../source/server/core/configuration/adapter/schema-for-process-env/schemaForProcessEnvWithAdapter.ts";
-import {z} from "zod";
 export const schemaForProcessEnvOfBuilding = schemaForProcessEnvWithAdapter;
--- development/core/schema-for-process-env/schemaForProcessEnvOfDevelopment.ts
+++ development/core/schema-for-process-env/schemaForProcessEnvOfDevelopment.ts
@@ -1,3 +1,2 @@
 import {schemaForProcessEnvWithServer} from "../../../source/server/core/configuration/server/schema-for-process-env/schemaForProcessEnvWithServer.ts";
-import {z} from "zod";
 export const schemaForProcessEnvOfDevelopment = schemaForProcessEnvWithServer;
